b941f92a83b4d500a1fe8d054028b6195329feee
Bedoy <carlos.bedoy@gmail.com>
2020-07-07 12:46:16 -0500

    First amazing coco commit

---
+++ app/src/main/java/iambedoy/coco/MainActivity.kt
@@ -0,0 +1,12 @@
+package iambedoy.coco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
